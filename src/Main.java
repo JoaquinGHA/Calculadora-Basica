e86b4b46635140785db83cfa92d6c3882664ccf2
Joaquin <joaquingabriel10@gmail.com>
2026-06-03 20:10:53 +0200

    Funciona con raíces y potencias

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,10 @@ public class Main {
 			System.out.println("2. Restar");
 			System.out.println("3. Multiplicar");
 			System.out.println("4. Dividir");
-			System.out.println("5. Reiniciar");
-			System.out.println("6. Mostrar historial");
+			System.out.println("5. Potencia");
+			System.out.println("6. Raíz");
+			System.out.println("7. Reiniciar");
+			System.out.println("8. Mostrar historial");
 			System.out.println("0. Salir del sistema");
 			opcion = sc.nextInt();
 			
@@ -38,7 +40,7 @@ public class Main {
 				double resultSuma = Operaciones.sumar(acumulador, b);
 				historial.add(acumulador+" + "+b+" = " +resultSuma);
 				acumulador = resultSuma;
-				System.out.println("Resultado: " +acumulador);
+				System.out.println("Resultado: " +String.format("%.2f", acumulador));
 				break;
 			case 2:
 				System.out.println("\nNúmero a restar: ");
@@ -46,7 +48,7 @@ public class Main {
 				double resultResta = Operaciones.restar(acumulador, b);
 				historial.add(acumulador+" - "+b+" = " +resultResta);
 				acumulador = resultResta;
-				System.out.println("Resultado: " +acumulador);
+				System.out.println("Resultado: " +String.format("%.2f", acumulador));
 				break;
 			case 3:
 				System.out.println("\nNúmero a multiplicar: ");
@@ -54,7 +56,7 @@ public class Main {
 				double resultMult = Operaciones.multiplicar(acumulador, b);
 				historial.add(acumulador+" * "+b+" = " +resultMult);
 				acumulador = resultMult;
-				System.out.println("Resultado: " +acumulador);
+				System.out.println("Resultado: " +String.format("%.2f", acumulador));
 				break;
 			case 4:
 				System.out.println("\nNúmero a dividir: ");
@@ -65,18 +67,39 @@ public class Main {
 					double resultDiv = Operaciones.dividir(acumulador, b);
 					historial.add(acumulador+ " / "+b+ " = "+resultDiv);
 					acumulador = resultDiv;
-					System.out.println("Resultado: " +acumulador);
+					System.out.println("Resultado: " +String.format("%.2f", acumulador));
 				}
 				
 				break;
-				
 			case 5:
+				System.out.println("\n¿Qué índice de raíz quieres aplicar a " + acumulador + "?");
+				b = sc.nextDouble();
+				double resultPot = Operaciones.potencia(acumulador, b);
+				historial.add(acumulador+" ^ "+b+" = "+resultPot);
+				acumulador = resultPot;
+				System.out.println ("Resultado: " +String.format("%.2f", acumulador));
+				break;
+				
+			case 6:
+				System.out.println("Ingrese la raíz que dese del número "+acumulador);
+				b  = sc.nextDouble();
+				if(b == 0) {
+					System.out.println("El índice no puede ser 0");}
+				else {
+					double resultRaiz = Operaciones.raiz(acumulador, b);
+					historial.add(acumulador + " raíz " + b + " = " + resultRaiz);
+					acumulador = resultRaiz;
+					System.out.println("Resultado: " +String.format("%.2f", acumulador));
+				}
+				break;
+				
+			case 7:
 				System.out.println("\n Ingresa el nuevo número: ");
 				acumulador = sc.nextDouble();
 				historial.add("----Reinicio del Sistema---Ingrese nuevo número --> " +acumulador+ " --");
 				System.out.println("Acumulador reiniciado a: "+acumulador);
 				break;
-			case 6:
+			case 8:
 				if(historial.isEmpty()) {
 					System.out.println("\nNo hay historial de operaciones");
 				}else {
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -5,5 +5,7 @@ public class Operaciones {
 	public static double restar(double a, double b) {return a-b;}
 	public static double multiplicar(double a, double b) {return a*b;}
 	public static double dividir(double a, double b) {return a/b;}
+	public static double potencia(double a, double b) {return Math.pow(a, b);}
+	public static double raiz(double a, double n) { return Math.pow(a, 1.0 / n); }
 
 }
